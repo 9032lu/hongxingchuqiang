22d81df654bae071df92dbeb349cff55ffde4b06
apple <apple@tang.local>
2017-09-13 10:34:10 +0800

    pdf

--- BletcShop/NewUserAccessCodeVC.m
+++ BletcShop/NewUserAccessCodeVC.m
@@ -185,6 +185,7 @@
     tf.returnKeyType=UIReturnKeyDone;
     tf.placeholder=@"请输入字符";
     tf.delegate=self;
+    tf.keyboardType=UIKeyboardTypeAlphabet;
     tf.borderStyle=UITextBorderStyleRoundedRect;
     [alert addSubview:tf];
     
@@ -205,6 +206,12 @@
 }
 -(BOOL)textFieldShouldReturn:(UITextField *)textField{
     [textField resignFirstResponder];
+    if (textField.text.length==5) {
+        //http://101.201.100.191/cnconsum/App/Extra/VerifyCode/check
+        [self assignImageCodeReuqst];
+    }else{
+        [self showHint:@"验证码长度不对"];
+    }
     return YES;
 }
 //刷新图片
@@ -255,6 +262,7 @@
                 });
             }else{
                 [self AccessImageCodeReuqst];
+                [self showHint:@"验证码错误"];
             }
         }
         
@@ -268,10 +276,6 @@
 
 -(void)valueChanged:(UITextField *)textFeild{
     NSLog(@"????????%@",textFeild.text);
-    if (textFeild.text.length==5) {
-        //http://101.201.100.191/cnconsum/App/Extra/VerifyCode/check
-        [self assignImageCodeReuqst];
-    }
 }
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
